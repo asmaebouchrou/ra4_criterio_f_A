5025e0ab03dda218e27f0e377ab6c3e6fdeb4e1f
emriesvc <emr.ies.vc@gmail.com>
2025-02-06 23:55:20 +0100

    V.0.0 Inicio de repositorio.

---
+++ src/Computer.java
@@ -0,0 +1,12 @@
+/**
+ * Clase computer que represanta a ordenadores en general
+ * @author Manuel Molino
+ * @version 1.0.0
+ */
+public class Computer {
+    private int ram;
+    private float cpu;
+    private String motherBoard;
+
+
+}
