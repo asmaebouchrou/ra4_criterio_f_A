7df24956eb63c38ac50b767a6240fca842ba05d8
asmaebouchrou <165679210+asmaebouchrou@users.noreply.github.com>
2025-02-07 14:00:34 +0100

    Documentación del toString

--- src/Computer.java
+++ src/Computer.java
@@ -7,7 +7,8 @@ public class Computer {
     private int ram;
     private float cpu;
     private String motherBoard;
-
+    /**
+     * Método toString que muestra los valores de cada una de las variables: ram, cpu, motherBoard*/
     @Override
     public String toString() {
         return "Computer{" +
