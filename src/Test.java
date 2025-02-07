ea4212b921c2af5d8e2378abd0304c36862f229d
asmaebouchrou <165679210+asmaebouchrou@users.noreply.github.com>
2025-02-07 14:10:48 +0100

    Documentada la clase test

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,12 @@
+/**
+ * Clase test para testear la clase computer
+ * @author Asmae Bouchrou
+ * @version 1.0.0
+ */
 public class Test {
     public static void main(String[] args) {
+        /**
+         * Creando estancias de computer con datos y testearlos*/
         Computer computer1 = new Computer(4,1000, "pepito");
         Computer computer2 = new Computer(16,28426, "pepe");
         System.out.println( computer1.toString());
